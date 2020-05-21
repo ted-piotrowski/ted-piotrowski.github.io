e72e9058ca780c55ac395443a557f736d40515c0
ted-piotrowski <ted@hopps.io>
2020-05-21 15:55:59 -0700

    history push state

--- client/App.tsx
+++ client/App.tsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Connection from './components/Connection';
 import './css/main.css';
 import { generateWords } from './utils/helpers';
 
 const App = () => {
+    const [room, setRoom] = useState(null);
     useEffect(() => {
         if (window.location.pathname === '/') {
-            window.location.href = `/${generateWords(3)}`;
+            history.replaceState(null, document.title, `/${generateWords(3)}`);
         }
+        setRoom(true);
     }, [])
 
-    if (window.location.pathname === '/') {
+    if (!room) {
         return null;
     }
     return <Connection />
